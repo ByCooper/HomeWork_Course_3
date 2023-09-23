e846ebe23b8797155261cba6d67a58cddcbb51c2
ByCooper <kup.official@yahoo.com>
2023-09-23 10:16:40 +0300

    Домашнее задание_2(4_курс)(CREATE + JOIN)(устранение замечаний)

--- src/main/resources/templates/script422.sql
+++ src/main/resources/templates/script422.sql
@@ -1,3 +1,10 @@
+CREATE TABLE car (
+    id INTEGER PRIMARY KEY,
+    mark VARCHAR(50),
+    model VARCHAR(50),
+    price NUMERIC CHECK (price > 0)
+);
+
 CREATE TABLE driver (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
@@ -6,30 +13,15 @@ CREATE TABLE driver (
     id_car INTEGER REFERENCES car(id)
 );
 
-CREATE TABLE car (
-    id INTEGER PRIMARY KEY,
-    mark VARCHAR(50),
-    model VARCHAR(50),
-    price NUMERIC CHECK (price > 0)
-);
 
 SELECT d.name, d.age, d.card, c.mark, c.model, c.price FROM driver AS d
-JOIN car AS c ON d.id_car = c.id;
+LEFT JOIN car AS c ON d.id_car = c.id;
 
 ---------------------------------------------------------------------------
-
-create table driver (
+create table price (
     id INTEGER primary key,
-    name TEXT NOT NULL,
-    age INTEGER CHECK (age > 0),
-    id_card INTEGER REFERENCES card(id),
-    id_car INTEGER REFERENCES auto(id)
-);
-
-create table card (
-    id INTEGER primary key,
-    age INTEGER CHECK (age > 0),
-    card BOOLEAN NOT NULL
+    model VARCHAR(50),
+    price NUMERIC CHECK (price > 0)
 );
 
 create table auto (
@@ -39,13 +31,21 @@ create table auto (
     id_price INTEGER REFERENCES price(id)
 );
 
-create table price (
+create table card (
     id INTEGER primary key,
-    model VARCHAR(50),
-    price NUMERIC CHECK (price > 0)
+    age INTEGER CHECK (age > 0),
+    card BOOLEAN NOT NULL
+);
+
+create table driver (
+    id INTEGER primary key,
+    name TEXT NOT NULL,
+    age INTEGER CHECK (age > 0),
+    id_card INTEGER REFERENCES card(id),
+    id_car INTEGER REFERENCES auto(id)
 );
 
 SELECT d.name, d.age, c.card, a.mark, a.model, p.price FROM driver AS d
-JOIN card AS c ON d.id_card = c.id
-JOIN auto AS a ON d.id_car = a.id
-JOIN price AS p ON a.id_price = p.id;
+LEFT JOIN card AS c ON d.id_card = c.id
+LEFT JOIN auto AS a ON d.id_car = a.id
+RIGHT JOIN price AS p ON a.id_price = p.id;

a3dc9f02a03172826c7a9e60be2d8ad4fc5ae811
anatoliy.kupriyanov <kup.official@yahoo.com>
2023-10-01 14:11:46 +0300

    Домашнее задание_4(4_курс)_Логирование + конфигурирование

--- src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
+import org.jboss.logging.Logger;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.FacultyRepository;
@@ -12,6 +14,7 @@ import java.util.stream.Collectors;
 @Service
 public class FacultyServiceImpl implements FacultyService{
 
+    private final Logger logger = LoggerFactory.logger(FacultyServiceImpl.class);
     private final FacultyRepository facultyRepository;
     private final StudentServiceImpl studentService;
 
@@ -22,11 +25,16 @@ public class FacultyServiceImpl implements FacultyService{
 
     @Override
     public Faculty add(Faculty faculty) {
+        logger.info("Was invoked method for add faculty");
         return facultyRepository.save(faculty);
     }
 
     @Override
     public Faculty update(Long id, Faculty faculty) {
+        logger.info("Was invoked method for update faculty");
+        if (id < 0 || find(id).equals(null) || !faculty.getClass().equals(Faculty.class)) {
+            logger.error("Error! Неправильно введенное значение");
+        }
         Faculty faculty1 = find(id);
         faculty1.setName(faculty.getName());
         faculty1.setColor(faculty.getColor());
@@ -35,16 +43,22 @@ public class FacultyServiceImpl implements FacultyService{
 
     @Override
     public Faculty find(Long id) {
+        logger.info("Was invoked method for find faculty");
+        if (id < 0 || facultyRepository.findById(id).isEmpty()) {
+            logger.error("Error! There is not faculty with id =" + id);
+        }
         return facultyRepository.findById(id).orElse(null);
     }
 
     @Override
     public void remove(Long id) {
+        logger.info("Was invoked method for remove faculty");
         facultyRepository.deleteById(id);
     }
 
     @Override
     public Collection<Faculty> filter(String color) {
+        logger.info("Was invoked method for filter faculty");
         return facultyRepository.findAll().stream()
                 .filter(e -> e.getColor().equals(color))
                 .collect(Collectors.toList());
@@ -52,19 +66,23 @@ public class FacultyServiceImpl implements FacultyService{
 
     @Override
     public Collection<Faculty> getAll() {
+        logger.info("Was invoked method for getAll faculty");
         return facultyRepository.findAll();
     }
 
     @Override
     public Collection<Faculty> findByNameContainsIgnoreCase(String name) {
+        logger.info("Was invoked method for findByNameContainsIgnoreCase faculty");
         return facultyRepository.findByNameContainsIgnoreCase(name);
     }
     @Override
     public Collection<Faculty> findByColorContainsIgnoreCase(String color) {
+        logger.info("Was invoked method for findByColorContainsIgnoreCase faculty");
         return facultyRepository.findByColorContainsIgnoreCase(color);
     }
     @Override
     public List<Student> getStudentFromFaculty(Long id) {
+        logger.info("Was invoked method for getStudentFromFaculty faculty");
         return studentService.findByFacultyId(id);
     }
 }
--- src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
+import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
@@ -12,6 +14,8 @@ import java.util.stream.Collectors;
 
 @Service
 public class StudentServiceImpl implements StudentService{
+
+    private final Logger logger = LoggerFactory.logger(StudentServiceImpl.class);
     @Autowired
     private final StudentRepository studentRepository;
 
@@ -21,26 +25,37 @@ public class StudentServiceImpl implements StudentService{
 
     @Override
     public Student add(Student student) {
+        logger.info("Was invoked method for add student");
         return studentRepository.save(student);
     }
 
     @Override
     public Student update(Student student) {
+        logger.info("Was invoked method for update student");
+        if (student.equals(null) || !student.getClass().equals(Student.class)) {
+            logger.error("There is not student with student = " + student);
+        }
         return studentRepository.save(student);
     }
 
     @Override
     public Student find(Long id) {
+        logger.info("Was invoked method for find student");
+        if (studentRepository.findById(id).isEmpty()) {
+            logger.error("There is not student with id = " + id);
+        }
         return studentRepository.findById(id).orElse(null);
     }
 
     @Override
     public void remove(Long id) {
+        logger.info("Was invoked method for remove student");
         studentRepository.deleteById(id);
     }
 
     @Override
     public Collection<Student> filter(int age) {
+        logger.info("Was invoked method for filter student");
         return studentRepository.findAll().stream()
                 .filter(e -> e.getAge() == age)
                 .collect(Collectors.toList());
@@ -48,30 +63,40 @@ public class StudentServiceImpl implements StudentService{
 
     @Override
     public Collection<Student> getAll() {
+        logger.info("Was invoked method for getAll student");
         return studentRepository.findAll();
     }
     @Override
     public Collection<Student> findByAgeBetween(int min, int max) {
+        logger.info("Was invoked method for findByAgeBetween student");
         return studentRepository.findByAgeBetween(min, max);
     }
     @Override
     public Faculty getFaculty(Long id) {
+        logger.info("Was invoked method for getFaculty student");
+        if (id < 0 || studentRepository.findById(id).isEmpty()) {
+            logger.error("There is not student with id = " + id);
+        }
         return studentRepository.findById(id).get().getFaculty();
     }
     @Override
     public List<Student> findByFacultyId(Long id) {
+        logger.info("Was invoked method for findByFacultyId student");
         return studentRepository.findByFaculty_Id(id);
     }
     @Override
     public Integer getStudentByCategory() {
+        logger.info("Was invoked method for getStudentByCategory student");
         return studentRepository.getStudentByCategory();
     }
     @Override
     public Integer getStudentByAvgAge() {
+        logger.info("Was invoked method for getStudentByAvgAge student");
         return studentRepository.getStudentByAvgAge();
     }
     @Override
     public List<StudentByCategory> getStudentByOffset() {
+        logger.info("Was invoked method for getStudentByOffset student");
         return studentRepository.getStudentByOffset();
     }
 }
